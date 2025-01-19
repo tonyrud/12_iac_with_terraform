2e4698a5553e1239b7ffd117b1c5db5aa2c9a1e1
tonyrud <tonrudny@gmail.com>
2025-01-19 13:21:47 -0500

    same step

--- modules/eks/argo.tf
+++ modules/eks/argo.tf
@@ -3,6 +3,8 @@ provider "helm" {
     host                   = data.aws_eks_cluster.this.endpoint
     token                  = data.aws_eks_cluster_auth.this.token
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority.0.data)
+    load_config_file       = false
+    
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
