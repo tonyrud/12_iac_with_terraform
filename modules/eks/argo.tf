d9745adf3dec55ae644961ca7731f3e82660eb99
tonyrud <tonrudny@gmail.com>
2025-01-19 14:03:02 -0500

    remove api token req

--- modules/eks/argo.tf
+++ modules/eks/argo.tf
@@ -4,11 +4,11 @@ provider "helm" {
     token                  = data.aws_eks_cluster_auth.this.token
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority.0.data)
     
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "aws"
-      args        = ["eks", "get-token", "--cluster-name",  module.eks.cluster_name]
-    }
+    # exec {
+    #   api_version = "client.authentication.k8s.io/v1beta1"
+    #   command     = "aws"
+    #   args        = ["eks", "get-token", "--cluster-name",  module.eks.cluster_name]
+    # }
   }
 }
 
