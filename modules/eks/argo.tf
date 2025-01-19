247c8a5bd43328cfaef3b167977a7e6a9474660f
tonyrud <tonrudny@gmail.com>
2025-01-19 13:33:48 -0500

    try wo kube config path

--- modules/eks/argo.tf
+++ modules/eks/argo.tf
@@ -3,7 +3,6 @@ provider "helm" {
     host                   = data.aws_eks_cluster.this.endpoint
     token                  = data.aws_eks_cluster_auth.this.token
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority.0.data)
-    load_config_file       = false
     
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
